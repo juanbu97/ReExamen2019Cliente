5acccedeec4c0800333b3d80e6e720d1d602964f
juanbu97 <juanbu97@gmail.com>
2020-01-14 18:03:26 +0100

    Clausula guarda close #10

--- JS/validar.js
+++ JS/validar.js
@@ -9,47 +9,44 @@ let Validar = {
     validarDni: function (dni) {
         const re = /^(\d{8})[ -]?([A-Za-z])$/;
         let cadena = "TRWAGMYFPDXBNJZSQVHLCKET";
+        dni = dni.trim();
 
-        if (re.test(dni.trim())) {
-            let [dniCompleto, numeroDni, letraDNI] = re.exec(dni.trim());
-            if (letraDNI.toUpperCase() != cadena[numeroDni % 23].toUpperCase()) {
-                return `El dni ${dniCompleto} no es correcto`;
-            } else {
-                return ``;
-            }
-        } else {
+        if (!re.test(dni))
             return `Formato dni incorrecto`;
-        }
 
-    }, validarNombre: function (nombre) {
+
+        let [dniCompleto, numeroDni, letraDNI] = re.exec(dni);
+
+        if (letraDNI.toUpperCase() !== cadena[numeroDni % 23].toUpperCase())
+            return `El dni ${dniCompleto} no es correcto`;
+        return ``;
+
+    },
+
+    validarNombre: function (nombre) {
         const re = /^[A-Za-zñóáéíúÁÉÍÓÚÑ]+[ ]?[A-Za-zñóáéíúÁÉÍÓÚÑ]+[ ]?[A-Za-zñóáéíúÁÉÍÓÚÑ]+$/;
-        if (re.test(nombre.trim())) {
-            return ``;
-        } else {
+        nombre = nombre.trim();
+        if (!re.test(nombre))
             return `Nombre incorrecto`;
+        return ``;
 
-        }
 
     }, validarFecha: function (fecha) {
         const re = /^(\d{2})([-/])(\d{2})\2(\d{4})$/;
+        fecha = fecha.trim();
 
-        if (!re.test(fecha)) {
+        if (!re.test(fecha))
             return "Error.La fecha debe tener el siguiente formato: dd/mm/aaaa o dd-mm-aaaa";
-        }
-        else {
 
-            let [, dia, , mes, anno] = re.exec(fecha);
-            dia = Number(dia);
-            mes = Number(mes);
-            anno = Number(anno);
-            let date = new Date(`${anno}/${mes}/${dia}`);
+        let [, dia, , mes, anno] = re.exec(fecha);
+        dia = Number(dia);
+        mes = Number(mes);
+        anno = Number(anno);
+        let date = new Date(`${anno}/${mes}/${dia}`);
 
-            if (date.getFullYear() === anno && date.getMonth() === mes - 1 && date.getDate() === dia) {
-                return "";
-            }
-            else {
-                return "La fecha introducida no es correcta.";
-            }
-        }
+        if (date.getFullYear() !== anno && date.getMonth() !== mes - 1 && date.getDate() !== dia)
+            return "La fecha introducida no es correcta.";
+        return "";
+
     }
 };
